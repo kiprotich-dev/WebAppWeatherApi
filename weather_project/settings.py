20fa6280410b13c3cb9da3972a1baf99f4283d63
kiprotich vincent <vinsntkiprotich@gmail.com>
2026-06-04 09:14:23 +0300

    allowed hosts

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -108,8 +108,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [BASE_DIR / "static,staticfiles"]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static,staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "weather.User"
