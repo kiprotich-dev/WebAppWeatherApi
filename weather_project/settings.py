fe4ccefc5a083f78917c20932a3aacfd494b9b3a
kiprotich vincent <vinsntkiprotich@gmail.com>
2026-06-04 08:37:45 +0300

    updateed static files

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -102,7 +102,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "weather.User"
