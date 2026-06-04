abec5ee2059fea8f9bb76b890f0fe369f84f75fc
kiprotich vincent <vinsntkiprotich@gmail.com>
2026-06-04 13:28:39 +0300

    allowed hosts

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -28,17 +28,17 @@ load_env_file(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-only-weather-dashboard-secret")
 DEBUG = env_bool("DJANGO_DEBUG", True)
-ALLOWED_HOSTS = [
-    host.strip()
-    for host in os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-    if host.strip()
-]
-
 # ALLOWED_HOSTS = [
-#     "127.0.0.1",
-#     "localhost",
-#     ".onrender.com"
+#     host.strip()
+#     for host in os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+#     if host.strip()
 # ]
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com"
+]
 
 
 INSTALLED_APPS = [
@@ -108,8 +108,9 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static",]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # STATIC_ROOT = os.path.join(BASE_DIR, "static,staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
